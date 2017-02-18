bdca6ba48d1996183fc3821781c9b5db8edd9355
Slava Buynov <SlavaB@playtika.com>
2017-02-18 15:32:49 +0300

    Cleanup...

--- PostfixCodeCompletion.Tests/Properties/AssemblyInfo.cs
+++ PostfixCodeCompletion.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SlavaRa")]
 [assembly: AssemblyProduct("PostfixCodeCompletion.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016 - 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
